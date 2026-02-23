f2e97278edffffd55c548e850b49fd412d32d597
T-Al-D <t.private@gmx.net>
2026-02-23 21:04:24 +0100

    chore: changed table CSS

--- src/pages/Benchmark_Dashboard.tsx
+++ src/pages/Benchmark_Dashboard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { supabase } from "../lib/supabase";
 
+// define the row in table
 type BenchmarkRun = {
   id: string;
   run_timestamp: string;
@@ -12,19 +13,23 @@ type BenchmarkRun = {
 };
 
 export function Feature_SnapShots_Dashboard() {
+  // state management
   const [data, setData] = useState<BenchmarkRun[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
+  // fetching the data
   async function fetchData() {
     setLoading(true);
     setError(null);
 
+    // send an HTTP rest to API, supabase is executing Query
+    // RLS is being checked, JSON response is send back
     const { data, error } = await supabase
       .from("service_feature_snapshots")
       .select("*")
       .order("run_timestamp", { ascending: false })
-      .limit(1000);
+      .limit(150);
 
     if (error) {
       setError(error.message);
@@ -32,12 +37,13 @@ export function Feature_SnapShots_Dashboard() {
       return;
     }
 
+    // save JSON response
     setData(data ?? []);
     setLoading(false);
   }
 
   return (
-    <div style={{ padding: "2rem" }}>
+    <div>
       <h1>Benchmark Dashboard</h1>
 
       <button onClick={fetchData} disabled={loading}>
@@ -46,10 +52,10 @@ export function Feature_SnapShots_Dashboard() {
       <br></br>
       <br></br>
 
-      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+      {error && <p className="status-down">Error: {error}</p>}
 
       {data.length > 0 && (
-        <table border={1} cellPadding={6}>
+        <table className="snapshot-table">
           <thead>
             <tr>
               <th>Timestamp</th>
@@ -61,8 +67,10 @@ export function Feature_SnapShots_Dashboard() {
             </tr>
           </thead>
           <tbody>
+            {/* iterrate over JSON and build DOM elements */}
             {data.map((row) => (
               <tr key={row.id}>
+                {/*table datacells inside a table row */}
                 <td>{new Date(row.run_timestamp).toLocaleString()}</td>
                 <td>{row.service}</td>
                 <td>{Number(row.p50_latency_s).toFixed(2)}</td>
